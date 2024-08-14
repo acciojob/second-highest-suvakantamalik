37c1d7008d8bb3e6a047793717beebef19648b89
suvakantamalik <134865642+suvakantamalik@users.noreply.github.com>
2024-08-14 17:59:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+//your JS code here. If required
+secondHighest(arr){
+	if(arr.legneth < 2) return -Infinity;
+	let first = -Infinity;
+	let second = -Infinity;
+	for(int i=0; i<arr.length; i++){
+		if(arr[i] > first){
+			second = first;
+			first = arr[i];
+		}
+		else if(arr[i] < first && arr[i] > second){
+			second = arr[i];
+		}
+	}
+	return second;
+}
